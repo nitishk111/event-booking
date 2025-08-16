58f3a8c8d6d23a90cef49b448cdb9601e59ac25b
Nitish <nitishchaudhary0609@gmail.com>
2025-08-16 22:42:56 +0530

    Tomcat server installed; welcome.jsp loading

---
+++ src/main/java/com/event/Servlets/LoginServlet.java
@@ -0,0 +1,22 @@
+package com.event.Servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login-servlet")
+public class LoginServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+
+		//check condition
+		System.out.println("service of loginPage");
+		resp.sendRedirect("views/home-profile.jsp");
+	}
+}
---
+++ src/main/webapp/views/welcome.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Event Registration Portal</title>
+</head>
+<body>
+<h1> Welcome </h1>
+
+	<form action="login-servlet">
+		Enter user name: <input type="text" name="userName"><br/>
+		Enter password: <input type="text" name="userPassword"><br/>
+		<input type="submit" value="Login">
+	</form>
+
+</body>
+</html>
